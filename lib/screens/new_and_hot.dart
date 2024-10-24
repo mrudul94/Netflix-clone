35460297c00bf20137294a064fbe1e77ef45af9f
Mrudul <mrudulp2002@gmail.com>
2024-10-24 21:38:04 +0530

    Initial commit

---
+++ lib/screens/new_and_hot.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NewAndHot extends StatelessWidget {
+  const NewAndHot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('new and hot'),
+      ),
+    );
+  }
+}
